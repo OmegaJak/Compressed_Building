5ed36cccbec90136d06f4ab28b97ade41e543ed5
WarlockJK <jak.kruger@gmail.com>
2013-11-18 20:00:56 -0600

    Switched from onItemUseFirst to onItemUse
    This fixed the issue with the initial block placed being left behind, I
    also had to add it in so that it would swing the item like it was a
    block, and added sounds when placed

--- common/warlockjk/compressedBuilding/items/ItemSquareCobblestone.java
+++ common/warlockjk/compressedBuilding/items/ItemSquareCobblestone.java
@@ -2,6 +2,7 @@ package warlockjk.compressedBuilding.items;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -13,7 +14,8 @@ public class ItemSquareCobblestone extends ItemBlock{
 	}
 	
 	@Override
-	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		player.swingItem();
 		if(!world.isRemote) {
 			PlacementUtil placementUtil = new PlacementUtil();	
 			placementUtil.placeBlocks(stack, player, world, x, y, z, side, Block.cobblestone.blockID, 1, 1);
@@ -21,6 +23,13 @@ public class ItemSquareCobblestone extends ItemBlock{
 				stack.stackSize--;
 			}
 		}
+		world.playSoundEffect((double)x + 0.5D, (double)y + 0.5D, (double)z + 0.5D, Block.soundStoneFootstep.getPlaceSound(), 1.0F, Block.soundStoneFootstep.getPitch() * 0.8F);
+		world.playSoundEffect((double)x + 0.5D, (double)y + 0.5D, (double)z + 0.5D, Block.soundStoneFootstep.getPlaceSound(), 1.0F, Block.soundStoneFootstep.getPitch() * 0.9F);
 		return false;
 	}
+	
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.block;
+    }
 }
