5dc13befd916d753932a897be0259b54a422a08f
OmegaJak <jak.kruger@gmail.com>
2014-02-25 22:58:17 -0600

    Began working to make shift-clicking work

--- src/main/java/com/omegajak/compressedbuilding/inventory/ContainerCompactor.java
+++ src/main/java/com/omegajak/compressedbuilding/inventory/ContainerCompactor.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 import com.omegajak.compressedbuilding.client.interfaces.SlotCompactor;
@@ -50,6 +51,30 @@ public class ContainerCompactor extends Container {
 	
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer player, int i) {
+		Slot slot = getSlot(i);
+		if (slot != null && slot.getHasStack()) {
+			ItemStack stack = slot.getStack();
+			ItemStack result = stack.copy();
+			
+			if (i >= 36) {
+				if (!mergeItemStack(stack, 0, 36, false)) {
+					return null;
+				}
+			}else if(!(stack.getItem() instanceof ItemBlock) || !mergeItemStack(stack, 36, 36 + compactor.getSizeInventory() - 1, false)) {
+				return null;
+			}
+			
+			if (stack.stackSize == 0) {
+				slot.putStack(null);
+			}else{
+				slot.onSlotChanged();
+			}
+			
+			slot.onPickupFromSlot(player, stack);
+			
+			return result;
+		}
+		
 		return null;
 	}
 
