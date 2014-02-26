9c07d3b4d4803a153383b00d8dd69053a713302b
OmegaJak <jak.kruger@gmail.com>
2014-02-26 16:00:39 -0600

    Fixed problem where diff itemStacks would stack
    This would happen when you shift clicked it, it was because hasSubtypes
    was false, allowing it to ignore what the damage was when mergeItemStack
    was called.

--- src/main/java/com/omegajak/compressedbuilding/items/ItemSquareTemplate.java
+++ src/main/java/com/omegajak/compressedbuilding/items/ItemSquareTemplate.java
@@ -14,6 +14,7 @@ import com.omegajak.compressedbuilding.utils.PlacementUtil;
 public class ItemSquareTemplate extends ItemBlock{
 	public ItemSquareTemplate(int id) {
 		super(id);
+		super.setHasSubtypes(true);
 	}
 	
 	@Override
