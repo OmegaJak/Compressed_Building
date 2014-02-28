bc6855d1472dc0dfda7d14f289a9d06ce72492af
OmegaJak <jak.kruger@gmail.com>
2014-02-28 11:25:28 -0600

    Revert "Dumb crap"
    This reverts commit 816929df3d58bb913a31e24b1a06a6f9c917e30f.

--- src/main/java/com/omegajak/compressedbuilding/proxies/ClientProxy.java
+++ src/main/java/com/omegajak/compressedbuilding/proxies/ClientProxy.java
@@ -1,24 +1,13 @@
 package com.omegajak.compressedbuilding.proxies;
 
-import cpw.mods.fml.client.registry.RenderingRegistry;
-
 public class ClientProxy extends CommonProxy{
-	
-	public static int renderPass;
-	public static int squareTemplateRenderType;
-	
 	@Override
 	public void initSounds() {
 		//init all the sounds
 	}
 	
-	public static void setCustomRenderers() {
-		squareTemplateRenderType = RenderingRegistry.getNextAvailableRenderId();
-		RenderingRegistry.registerBlockHandler(new SquareTemplateRenderer());
-	}
-	
 	@Override
 	public void initRenderers() {
-		DoubleRenderClientProxy.setCustomRenderers();
+		
 	}
 }
