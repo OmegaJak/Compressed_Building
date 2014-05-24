364f47a34f8d08cd937a527440b4a21b43bd31b1
OmegaJak <jak.kruger@gmail.com>
2014-05-23 19:57:54 -0500

    Trying to fix rendering weirdness

--- src/main/java/com/omegajak/compressedbuilding/client/RenderItemSquareTemplate.java
+++ src/main/java/com/omegajak/compressedbuilding/client/RenderItemSquareTemplate.java
@@ -73,6 +73,9 @@ public class RenderItemSquareTemplate implements IItemRenderer{
 	      default:
 	        break; // never here
 	    }
+		 
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		
 		// xpos face
 	    tessellator.setNormal(1.0F, 0.0F, 0.0F);
@@ -118,6 +121,7 @@ public class RenderItemSquareTemplate implements IItemRenderer{
 
 	    tessellator.draw();
 	    
+	    GL11.glDisable(GL11.GL_BLEND);
 	   
 	    //Render the inner block
 		
